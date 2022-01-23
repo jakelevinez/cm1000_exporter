20495ea3c7b0b146a14186bb3960ef2983d46bcc
jakelevinez <31458570+jakelevinez@users.noreply.github.com>
2022-01-23 11:16:15 -0500

    init setup

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
 import (
- "github.com/prometheus/client_golang/prometheus"
- "github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"log"
+	"net/http"
 )
+
+func main() {
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(":9527", nil))
+}
