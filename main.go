66f95cadd951b44621d6d092fef12098de18cdab
Jake Levine <jlevine@genesistrading.com>
2022-03-25 22:25:06 -0400

    initialization of defaults

--- main.go
+++ main.go
@@ -1,12 +1,37 @@
 package main
 
 import (
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+type Modem struct {
+	Url  string `default:"192.168.100.1"`
+	User string `default:"admin"`
+	Pass string `default:"password"`
+}
+
 func main() {
+
 	http.Handle("/metrics", promhttp.Handler())
 	log.Fatal(http.ListenAndServe(":9527", nil))
+
+	currentModem := Modem{}
+
+	url, existsUrl := os.LookupEnv("MODEM_URL")
+	user, existsUser := os.LookupEnv("MODEM_USER")
+	pass, existsPass := os.LookupEnv("MODEM_PASS")
+	if existsUrl {
+		currentModem.Url = url
+	}
+	if existsUser {
+		currentModem.User = user
+	}
+	if existsPass {
+		currentModem.Pass = pass
+	}
+
 }
