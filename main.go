6d055a991ed7ca51fede2244f3386ece46d289d8
Jake Levine <jlevine@genesistrading.com>
2022-04-10 13:25:43 -0400

    got base metrics working

--- main.go
+++ main.go
@@ -37,47 +37,43 @@ type webToken struct {
 }
 
 type dsTable struct {
-	channel                 int
 	lock_status             string
 	modulation              string
-	channel_id              int
-	frequency               int
+	channel_id              string
+	frequency               float64
 	power                   float64
 	snr_mer                 float64
-	unerrored_codewords     int
-	correctable_codewords   int
-	uncorrectable_codewords int
+	unerrored_codewords     float64
+	correctable_codewords   float64
+	uncorrectable_codewords float64
 }
 
 type usTable struct {
-	channel     int
 	lock_status string
 	modulation  string
-	channel_id  int
-	frequency   int
+	channel_id  string
+	frequency   float64
 	power       float64
 }
 
 type dsOFDMTable struct {
-	channel                        int
 	lock_status                    string
 	modulation_profile_id          string
-	channel_id                     int
-	frequency                      int
+	channel_id                     string
+	frequency                      float64
 	power                          float64
 	snr_mer                        float64
 	active_subcarrier_number_range string
-	unerrored_codewords            int
-	correctable_codewords          int
-	uncorrectable_codewords        int
+	unerrored_codewords            float64
+	correctable_codewords          float64
+	uncorrectable_codewords        float64
 }
 
 type usOFDMATable struct {
-	channel               int
 	lock_status           string
 	modulation_profile_id string
-	channel_id            int
-	frequency             int
+	channel_id            string
+	frequency             float64
 	power                 float64
 }
 
@@ -183,30 +179,54 @@ var (
 		Help: "The total number of unsuccessful modem scrapes",
 	})
 )
+var (
+	channel_frequency = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "channel_frequency_hz",
+		Help: "Channel Frequency",
+	},
+		[]string{"channel", "channel_type", "direction"})
+)
+var (
+	channel_power = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "channel_power_dbmv",
+		Help: "Channel power",
+	},
+		[]string{"channel", "channel_type", "direction"})
+)
+var (
+	channel_snr_mer = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "channel_snr_mer",
+		Help: "Channel SNR MER",
+	},
+		[]string{"channel", "channel_type", "direction"})
+)
+var (
+	channel_unerrored_codewords = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "channel_unerrored_codewords",
+		Help: "The number of unerrored codewords",
+	},
+		[]string{"channel", "channel_type", "direction"})
+)
+var (
+	channel_correctable_codewords = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "channel_correctable_codewords",
+		Help: "The number of correctable codewords",
+	},
+		[]string{"channel", "channel_type", "direction"})
+)
+var (
+	channel_uncorrectable_codewords = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "channel_uncorrectable_codewords",
+		Help: "The number of uncorrectable codewords",
+	},
+		[]string{"channel", "channel_type", "direction"})
+)
 
-func convertTabletoI(table *goquery.Selection, i int) int {
+func convertTabletoFloat(table *goquery.Selection, i int) float64 {
 	//takes a goquery selection and a row index and returns the value of the cell at that index as an integer.
 
-	var return_value int
-	if value, err := strconv.Atoi(table.Find("td").Eq(i).Text()); err == nil {
-		return_value = value
-	} else if err != nil {
-		log.Fatalln(err)
-	}
-
-	return return_value
-}
-
-func convertStringTabletoI(table *goquery.Selection, i int, st string) int {
-	//takes a goquery selection, row index, and string to replace and returns the value of the cell at that index as an integer.
-	//removes spaces before parsing
-
-	var return_value int
-
-	parsed_value_with_spaces := strings.ReplaceAll(table.Find("td").Eq(i).Text(), st, "")
-	parsed_value := strings.ReplaceAll(parsed_value_with_spaces, " ", "")
-
-	if value, err := strconv.Atoi(parsed_value); err == nil {
+	var return_value float64
+	if value, err := strconv.ParseFloat(table.Find("td").Eq(i).Text(), 64); err == nil {
 		return_value = value
 	} else if err != nil {
 		log.Fatalln(err)
@@ -247,63 +267,23 @@ func exportMetrics(scrapeData *goquery.Document, initialRun bool) {
 	downstreamTable.Each(func(i int, s *goquery.Selection) {
 
 		dsTableData := dsTable{
-			channel:                 convertTabletoI(s, 0),
 			lock_status:             s.Find("td").Eq(1).Text(),
 			modulation:              s.Find("td").Eq(2).Text(),
-			channel_id:              convertTabletoI(s, 3),
-			frequency:               convertStringTabletoI(s, 4, "Hz"),
+			channel_id:              s.Find("td").Eq(3).Text(),
+			frequency:               convertStringTabletoFloat(s, 4, "Hz"),
 			power:                   convertStringTabletoFloat(s, 5, "dBmV"),
 			snr_mer:                 convertStringTabletoFloat(s, 6, "dB"),
-			unerrored_codewords:     convertTabletoI(s, 7),
-			correctable_codewords:   convertTabletoI(s, 8),
-			uncorrectable_codewords: convertTabletoI(s, 9),
+			unerrored_codewords:     convertTabletoFloat(s, 7),
+			correctable_codewords:   convertTabletoFloat(s, 8),
+			uncorrectable_codewords: convertTabletoFloat(s, 9),
 		}
 
-		if initialRun {
-
-			var (
-				channel_lock_status = promauto.NewGauge(prometheus.GaugeOpts{
-					Name: "channel_" + strconv.Itoa(dsTableData.channel) + "_lock_status",
-					Help: "Channel lock status",
-				})
-			)
-			var (
-				channel_power = promauto.NewGauge(prometheus.GaugeOpts{
-					Name: "channel_" + strconv.Itoa(dsTableData.channel) + "_power",
-					Help: "Channel power",
-				})
-			)
-			var (
-				channel_snr_mer = promauto.NewGauge(prometheus.GaugeOpts{
-					Name: "channel_" + strconv.Itoa(dsTableData.channel) + "_snr_mer",
-					Help: "Channel SNR MER",
-				})
-			)
-			var (
-				channel_unerrored_codewords = promauto.NewCounter(prometheus.CounterOpts{
-					Name: "channel_" + strconv.Itoa(dsTableData.channel) + "_unerrored_codewords",
-					Help: "The number of unerrored codewords",
-				})
-			)
-			channel_unerrored_codewords.
-			var (
-				channel_correctable_codewords = promauto.NewCounter(prometheus.CounterOpts{
-					Name: "channel_" + strconv.Itoa(dsTableData.channel) + "_correctable_codewords",
-					Help: "The number of correctable codewords",
-				})
-			)
-			var (
-				channel_uncorrectable_codewords = promauto.NewCounter(prometheus.CounterOpts{
-					Name: "channel_" + strconv.Itoa(dsTableData.channel) + "_uncorrectable_codewords",
-					Help: "The number of uncorrectable codewords",
-				})
-			)
-
-		} else {
-
-		}
-
-		fmt.Printf("%v \n", dsTableData)
+		channel_power.With(prometheus.Labels{"channel": dsTableData.channel_id, "direction": "downstream", "channel_type": "bonded"}).Set(dsTableData.power)
+		channel_snr_mer.With(prometheus.Labels{"channel": dsTableData.channel_id, "direction": "downstream", "channel_type": "bonded"}).Set(dsTableData.snr_mer)
+		channel_unerrored_codewords.With(prometheus.Labels{"channel": dsTableData.channel_id, "direction": "downstream", "channel_type": "bonded"}).Set(dsTableData.unerrored_codewords)
+		channel_correctable_codewords.With(prometheus.Labels{"channel": dsTableData.channel_id, "direction": "downstream", "channel_type": "bonded"}).Set(dsTableData.correctable_codewords)
+		channel_uncorrectable_codewords.With(prometheus.Labels{"channel": dsTableData.channel_id, "direction": "downstream", "channel_type": "bonded"}).Set(dsTableData.uncorrectable_codewords)
+		channel_frequency.With(prometheus.Labels{"channel": dsTableData.channel_id, "direction": "downstream", "channel_type": "bonded"}).Set(float64(dsTableData.frequency))
 
 	})
 
@@ -312,19 +292,15 @@ func exportMetrics(scrapeData *goquery.Document, initialRun bool) {
 	upstreamTable.Each(func(i int, s *goquery.Selection) {
 
 		usTableData := usTable{
-			channel:     convertTabletoI(s, 0),
 			lock_status: s.Find("td").Eq(1).Text(),
 			modulation:  s.Find("td").Eq(2).Text(),
-			channel_id:  convertTabletoI(s, 3),
-			frequency:   convertStringTabletoI(s, 4, "Hz"),
+			channel_id:  s.Find("td").Eq(3).Text(),
+			frequency:   convertStringTabletoFloat(s, 4, "Hz"),
 			power:       convertStringTabletoFloat(s, 5, "dBmV"),
 		}
-		if initialRun {
 
-		} else {
-
-		}
-		fmt.Printf("%v \n", usTableData)
+		channel_power.With(prometheus.Labels{"channel": usTableData.channel_id, "direction": "upstream", "channel_type": "bonded"}).Set(usTableData.power)
+		channel_frequency.With(prometheus.Labels{"channel": usTableData.channel_id, "direction": "upstream", "channel_type": "bonded"}).Set(usTableData.frequency)
 
 	})
 
@@ -333,25 +309,24 @@ func exportMetrics(scrapeData *goquery.Document, initialRun bool) {
 	downstreamOFDMTable.Each(func(i int, s *goquery.Selection) {
 
 		dsOFDMTableData := dsOFDMTable{
-			channel:                        convertTabletoI(s, 0),
 			lock_status:                    s.Find("td").Eq(1).Text(),
 			modulation_profile_id:          s.Find("td").Eq(2).Text(),
-			channel_id:                     convertTabletoI(s, 3),
-			frequency:                      convertStringTabletoI(s, 4, "Hz"),
+			channel_id:                     s.Find("td").Eq(3).Text(),
+			frequency:                      convertStringTabletoFloat(s, 4, "Hz"),
 			power:                          convertStringTabletoFloat(s, 5, "dBmV"),
 			snr_mer:                        convertStringTabletoFloat(s, 6, "dB"),
 			active_subcarrier_number_range: s.Find("td").Eq(7).Text(),
-			unerrored_codewords:            convertTabletoI(s, 8),
-			correctable_codewords:          convertTabletoI(s, 9),
-			uncorrectable_codewords:        convertTabletoI(s, 10),
-		}
-		if initialRun {
-
-		} else {
-
+			unerrored_codewords:            convertTabletoFloat(s, 8),
+			correctable_codewords:          convertTabletoFloat(s, 9),
+			uncorrectable_codewords:        convertTabletoFloat(s, 10),
 		}
 
-		fmt.Printf("%v \n", dsOFDMTableData)
+		channel_power.With(prometheus.Labels{"channel": dsOFDMTableData.channel_id, "direction": "downstream", "channel_type": "ofdm"}).Set(dsOFDMTableData.power)
+		channel_snr_mer.With(prometheus.Labels{"channel": dsOFDMTableData.channel_id, "direction": "downstream", "channel_type": "ofdm"}).Set(dsOFDMTableData.snr_mer)
+		channel_unerrored_codewords.With(prometheus.Labels{"channel": dsOFDMTableData.channel_id, "direction": "downstream", "channel_type": "ofdm"}).Set(dsOFDMTableData.unerrored_codewords)
+		channel_correctable_codewords.With(prometheus.Labels{"channel": dsOFDMTableData.channel_id, "direction": "downstream", "channel_type": "ofdm"}).Set(dsOFDMTableData.correctable_codewords)
+		channel_uncorrectable_codewords.With(prometheus.Labels{"channel": dsOFDMTableData.channel_id, "direction": "downstream", "channel_type": "ofdm"}).Set(dsOFDMTableData.uncorrectable_codewords)
+		channel_frequency.With(prometheus.Labels{"channel": dsOFDMTableData.channel_id, "direction": "downstream", "channel_type": "ofdm"}).Set(float64(dsOFDMTableData.frequency))
 
 	})
 
@@ -359,20 +334,15 @@ func exportMetrics(scrapeData *goquery.Document, initialRun bool) {
 
 	upstreamOFDMATable.Each(func(i int, s *goquery.Selection) {
 		usOFDMATableData := usOFDMATable{
-			channel:               convertTabletoI(s, 0),
 			lock_status:           s.Find("td").Eq(1).Text(),
 			modulation_profile_id: s.Find("td").Eq(2).Text(),
-			channel_id:            convertTabletoI(s, 3),
-			frequency:             convertStringTabletoI(s, 4, "Hz"),
+			channel_id:            s.Find("td").Eq(3).Text(),
+			frequency:             convertStringTabletoFloat(s, 4, "Hz"),
 			power:                 convertStringTabletoFloat(s, 5, "dBmV"),
 		}
-		if initialRun {
 
-		} else {
-
-		}
-
-		fmt.Printf("%v \n", usOFDMATableData)
+		channel_frequency.With(prometheus.Labels{"channel": usOFDMATableData.channel_id, "direction": "upstream", "channel_type": "ofdma"}).Set(usOFDMATableData.frequency)
+		channel_power.With(prometheus.Labels{"channel": usOFDMATableData.channel_id, "direction": "upstream", "channel_type": "ofdma"}).Set(usOFDMATableData.power)
 	})
 }
 
